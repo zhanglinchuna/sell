641f625d6c983203b69f706525a3c7c74e7cdc6a
zhanglinchun <2601165134@qq.com>
2019-10-13 22:38:41 +0800

    第一次提交

---
+++ src/main/java/com/zhanglinchun/sell/repository/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.zhanglinchun.sell.repository;
+
+import com.zhanglinchun.sell.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+}
---
+++ src/test/java/com/zhanglinchun/sell/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.zhanglinchun.sell.repository;
+
+import com.zhanglinchun.sell.dataobject.ProductCategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Test
+    public void findOneTest() {
+        ProductCategory productCategory = repository.findOne(1);
+        System.out.println(productCategory.toString());
+    }
+
+    @Test
+    public void saveTest() {
+        ProductCategory productCategory = new ProductCategory("女生最爱", 2);
+        ProductCategory result = repository.save(productCategory);
+    }
+}
